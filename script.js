74bb66d51d00832aae221a4dfcb76a3684713c58
raposinha123 <ana.almeida.rocha@escola.pr.gov.br>
2025-09-25 11:43:28 -0300

    ...

---
+++ script.js
@@ -0,0 +1,12 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  alert('Fui clicado!');
+}const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+}
